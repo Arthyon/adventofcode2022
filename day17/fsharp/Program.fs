de49d5c25e6cbbf58d377d05dfd0ad915643b3ea
Christian Hochlin <christian@hochlin.no>
2023-01-02 14:27:35 +0100

    day17: Add part 2

--- day17/fsharp/Program.fs
+++ day17/fsharp/Program.fs
@@ -10,7 +10,7 @@ let shapes = [
     [ (0, 1); (1, 1); (0, 0); (1, 0) ]  // .
 ]
 
-let input = File.ReadAllText("input").ToCharArray()
+let input = File.ReadAllText("input").ToCharArray() |> Seq.indexed
 let movement = seq { while true do yield! input }
 
 let spawn height shape = shape |> List.map (fun (x,y) -> x + 2, y + height + 3)
@@ -31,16 +31,16 @@ let canMoveRock rock tower =
         rock |> Seq.exists (fun point -> Set.contains point tower || outOfBounds point) |> not
         
         
-let rec fall tower rock (movement: IEnumerator<char>) =
+let rec fall tower rock (movement: IEnumerator<int * char>) =
         movement.MoveNext() |> ignore
-        let nextMovement = movement.Current
+        let idx,nextMovement = movement.Current
         
         let nextRock = jetstream rock nextMovement
         let rock = if canMoveRock nextRock tower then nextRock else rock
         let fallenRock = rock |> List.map (fun (x,y) -> x, y - 1)
         match canMoveRock fallenRock tower with
         | false ->
-                Set.union (Set.ofList rock) tower
+                (Set.union (Set.ofList rock) tower), idx
         | true  ->
                 fall tower fallenRock movement
         
@@ -55,9 +55,40 @@ let movementEnumerator = movement.GetEnumerator()
 let mutable tower = Set.empty
 for i = 0 to 2021 do
       let nextShape = shapes[i % 5]
-      tower <- rockfall tower nextShape movementEnumerator
+      let newTower, _ = rockfall tower nextShape movementEnumerator
+      tower <- newTower
       
         
 tower |> Seq.map snd |> Seq.max |> (+) 1 |> printfn "%A"
 
-// 3142 too high
+// Part 2
+let mutable tower2 = Set.empty
+let mutable rocks = 0UL
+let mutable rockShape = -1
+let movementEnumerator2 = movement.GetEnumerator()
+let mutable seen = Map.empty
+let mutable addedByRepeating = 0UL
+let mutable target = 1000000000000UL
+
+while rocks <= target do
+      rockShape <- (rockShape + 1) % 5
+      let nextShape = shapes[rockShape]
+      let newTower, idx = rockfall tower2 nextShape movementEnumerator2
+      tower2 <- newTower
+      
+      let key = rockShape,idx
+      let top = tower2 |> Seq.map snd |> Seq.max
+      let repeats,oldPieces,oldTop = if seen.ContainsKey(key) then seen[key] else 0,0UL,0
+      if addedByRepeating = 0UL then
+              if repeats = 3 then
+                      let deltaTop = top - oldTop
+                      let deltaPieces = rocks - oldPieces
+                      let repeats = (target - rocks) / uint64 deltaPieces
+                      addedByRepeating <- repeats * (uint64 deltaTop)
+                      rocks <- rocks + (repeats * (uint64 deltaPieces))
+                      
+              seen <- Map.add key (repeats + 1, rocks, top) seen
+              
+      rocks <- rocks + 1UL
+        
+tower2 |> Seq.map snd |> Seq.map uint64 |> Seq.max |> (+) addedByRepeating |> printfn "%A"
